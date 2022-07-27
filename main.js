16af5fed54d0522cd72cc5659f5d93f1bb844188
dk-dev10 <webdev102203@gmail.com>
2022-07-27 16:06:12 +0300

    all function working with localstorage

--- main.js
+++ main.js
@@ -1,22 +1,4 @@
-let tasks = [
-  {
-    title: 'Купить',
-    id: 12314,
-    done: false,
-  }, {
-    title: 'Оплатить',
-    id: 23455,
-    done: false,
-  }, {
-    title: 'Получить',
-    id: 76762,
-    done: false,
-  }, {
-    title: 'Бежать',
-    id: 76722,
-    done: true,
-  }
-];
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 const addTodo = document.querySelector('.addTodo');
 const addTodoInput = document.querySelector('input');
@@ -25,6 +7,9 @@ const addTodoButton = document.querySelector('button');
 const tasksProgress = document.querySelector('.tasks__progress');
 const tasksSuccess = document.querySelector('.tasks__success');
 
+function setLS(data) {
+  localStorage.setItem('tasks', JSON.stringify(data))
+}
 function newTodo(val) {
   const todo = {
     title: val,
@@ -32,17 +17,29 @@ function newTodo(val) {
     done: false,
   }
   tasks.push(todo);
+  setLS(tasks);
   template(tasksProgress, todo)
+  render()
 }
 
 function render() {
   const progress = tasks.filter((todo) => !todo.done);
   const success = tasks.filter((todo) => todo.done);
 
-  tasksProgress.innerHTML = ''
-  tasksSuccess.innerHTML = ''
-  progress.forEach(todo => template(tasksProgress, todo));
-  success.forEach(todo => template(tasksSuccess, todo));
+  if (!!progress.length) {
+    tasksProgress.innerHTML = ''
+    progress.forEach(todo => template(tasksProgress, todo));
+  } else {
+    tasksProgress.innerHTML = '<span>Пусто</span>';
+  }
+
+  if (!!success.length) {
+    tasksSuccess.innerHTML = ''
+    success.forEach(todo => template(tasksSuccess, todo));
+  } else {
+    tasksSuccess.innerHTML = '<span>Пусто</span>';
+  }
+
 }
 
 function template(where, todo) {
@@ -64,26 +61,29 @@ function template(where, todo) {
 
 function changeTodo(id) {
   tasks = tasks.map((todo) => todo.id === +id ? { ...todo, done: !todo.done } : todo);
+  setLS(tasks)
   render();
 }
 
 function removeTodo(id) {
   tasks = tasks.filter((todo) => todo.id !== +id);
+  setLS(tasks)
   render();
 }
 
 let value;
 addTodoInput.addEventListener('input', (e) => value = e.target.value);
-addTodoButton.addEventListener('click', () => {
+addTodo.addEventListener('submit', (e) => {
+  e.preventDefault();
   if (!!value) {
     newTodo(value);
     addTodoInput.value = value = '';
+
   }
 });
 render();
 
 const tasksProgressTodo = document.querySelector('.tasks');
-
 
 tasksProgressTodo.addEventListener('click', (e) => {
   if (e.target.nodeName === 'LABEL') {
@@ -98,4 +98,13 @@ tasksProgressTodo.addEventListener('click', (e) => {
 })
 
 
+tasksProgress.addEventListener('click', (e) => {
+  tasksSuccess.className = 'tasks__success close';
+  tasksProgress.className = 'tasks__progress open';
+})
+
+tasksSuccess.addEventListener('click', (e) => {
+  tasksProgress.className = 'tasks__progress close ';
+  tasksSuccess.className = 'tasks__success open ';
+})
 
